8b09d90647f9106e3f3aa12afebad212c7ec97ab
mehedi556 <mehe556di@gmail.com>
2022-11-10 07:32:28 +0600

    adding mongodb

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 //middleware----------
 
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.emwzks8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 app.get('/' , (req , res) => {
