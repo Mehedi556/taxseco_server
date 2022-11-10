a319e929ca04e811e34e0adcda2a77022fd8e478
mehedi556 <mehe556di@gmail.com>
2022-11-10 22:35:04 +0600

    adding addService api

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ async function run(){
         const serviceCollection = client.db('taxseco').collection('services');
         const reviewCollection = client.db('taxseco').collection('reviews');
 
-       
+        app.post('/service' , async(req , res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
 
         app.get('/services' , async(req , res) => {
             const query = {};
